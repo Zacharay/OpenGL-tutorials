1269369989437d11b8bf7e6ad636a846f46d5c7d
Zachariasz Słupski <zachary.slupski@wp.pl>
2023-07-26 09:42:09 +0200

    rendering square of the two triangles

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -2,6 +2,49 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+
+struct ShaderSources
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderSources ParseShader(const std::string& filePath)
+{
+    std::ifstream stream(filePath);
+
+    enum class ShaderType {
+        NONE=-1,
+        VERTEX=0,
+        FRAGMENT=1
+    };
+    std::stringstream ss[2];
+    std::string line;
+    ShaderType type = ShaderType::NONE;
+
+
+    while (getline(stream, line))
+    {
+        if (line.find("#shader") != std::string::npos)
+        {
+            if (line.find("vertex") != std::string::npos)
+            {
+                type = ShaderType::VERTEX;
+            }
+            else if (line.find("fragment") != std::string::npos)
+            {
+                type = ShaderType::FRAGMENT;
+            }
+        }
+        else {
+            ss[(int)type] << line << '\n';
+        }
+    }
+
+    return { ss[0].str(),ss[1].str() };
+}
 
 static unsigned int CompileShader(unsigned int type,const std::string& sourceCode)
 {
@@ -67,43 +110,31 @@ int main(void)
     {
         std::cout << "Glew not ok" << std::endl;
     }
-    float positions[6] = {
+    float positions[12] = {
         0.5f, -0.5f,
-        0.0f, 0.5f,
-        -0.5f,-0.5f
+        0.5F, 0.5f,
+        -0.5f,-0.5f,
+        -0.5f,-0.5f,
+        0.5F, 0.5f,
+        -0.5f,0.5f
     };
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions,GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), positions,GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
     glEnableVertexAttribArray(0);
 
-    std::string vertexShader = R"glsl(
-    #version 330 core
-    layout(location=0)in vec4 position;
-    void main()
-    {
-        gl_Position =  position;
-    }
-    )glsl";
-    std::string fragmentShader = R"glsl(
-    #version 330 core
-    layout(location=0)out vec4 color;
-    void main()
-    {
-        color = vec4(1.0,0.0,0.0,1.0);
-    }
-    )glsl";
+    ShaderSources source = ParseShader("res/shaders/Basic.shader");
 
-    unsigned int shader = CreateShader(vertexShader,fragmentShader);
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES,0,3);
+        glDrawArrays(GL_TRIANGLES ,0, 6);
 
         glfwSwapBuffers(window);
 
